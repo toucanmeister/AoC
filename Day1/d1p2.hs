b372d3f644c7fc3c1b6bf50c02a73c306146805a
Farin Lippmann <toucanmeister@gmail.com>
2022-12-03 08:51:12 +0100

    Init: Day1 and Day2

---
+++ Day1/d1p2.hs
@@ -0,0 +1,24 @@
+import Control.Monad
+import Data.Char
+import Data.List
+import System.IO
+
+main = do
+    handle <- openFile "data.txt" ReadMode
+    contents <- hGetContents handle
+    putStrLn . show . solveProblem $ contents
+    hClose handle
+
+solveProblem :: String -> Int
+solveProblem = sum . take 3 . reverse . sort . sumByElf . map toInt . lines
+
+toInt :: String -> Int
+toInt "" = 0
+toInt s  = read s
+
+sumByElf :: [Int] -> [Int]
+sumByElf input = 
+    let (x,l) = foldr combine (0,[]) input
+    in x:l
+    where combine 0 (acc,l) = (0,acc:l)
+          combine n (acc,l) = (acc+n,l)
---
+++ Day2/d2p2.hs
@@ -0,0 +1,22 @@
+import System.IO
+
+main = do
+  handle <- openFile "input.txt" ReadMode
+  contents <- hGetContents handle
+  putStrLn . show . solveProblem $ contents
+  hClose handle
+
+solveProblem :: String -> Int
+solveProblem = sum . map calculateScore . lines
+  where
+    calculateScore :: String -> Int
+    calculateScore "A X" = 3+0
+    calculateScore "A Y" = 1+3
+    calculateScore "A Z" = 2+6
+    calculateScore "B X" = 1+0
+    calculateScore "B Y" = 2+3
+    calculateScore "B Z" = 3+6
+    calculateScore "C X" = 2+0
+    calculateScore "C Y" = 3+3
+    calculateScore "C Z" = 1+6
+    calculateScore _ = 0
